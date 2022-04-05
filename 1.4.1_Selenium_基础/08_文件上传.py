f60ae291453b503f6b673440fc8617032cdf5388
Administrator <z843395332@163.com>
2022-04-05 10:21:19 +0800

    增加文件

--- 1.4.1_Selenium_基础/08_文件上传.py
+++ 1.4.1_Selenium_基础/08_文件上传.py
@@ -10,15 +10,15 @@ class Test_load():
 
     def teardown(self):
         self.driver.quit()
-
+# 文件上传时，和正常的input输入框类似，区别在于传入的值为上传文件所在路径
     def test_upload(self):
         self.driver.get("http://www.baidu.com")
         self.driver.find_element(By.CSS_SELECTOR, ".soutu-btn").click()
         sleep(5)
         #使用XPATH方式定位上传文件按钮
         # self.driver.find_element(By.XPATH, "//*[@id='form']/div/div[2]/div[2]/input").send_keys(
-        #     "E:/Python自动化基础2022/1.4_Selenium_基础/image/1.jpg")
+        #     "E:/Python自动化基础2022/1.4.1_Selenium_基础/image/1.jpg")
         # 使用CSS_SELECTOR方式定位上传文件按钮
         self.driver.find_element(By.CSS_SELECTOR, ".upload-pic").send_keys(
-            "E:/Python自动化基础2022/1.4_Selenium_基础/image/1.jpg")
+            "E:/Python自动化基础2022/1.4.1_Selenium_基础/image/1.jpg")
         sleep(10)
---
+++ 1.4.2_Appium_基础/01_Appium连接模拟器测试.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '7.1.2'
+desired_caps['deviceName'] = '127.0.0.1:62001'
+desired_caps['appPackage'] = 'com.taobao.taobao'
+desired_caps['appActivity'] = 'com.taobao.tao.TBMainActivity'
+desired_caps['norest'] = 'true'
+driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+
+el1 = driver.find_element_by_xpath("//android.widget.FrameLayout[@content-desc=\"聚划算商品2\"]/android.widget.ImageView[1]")
+driver.implicitly_wait(60)
+el1.click()
+
+TouchAction(driver).press(x=494, y=1817).move_to(x=519, y=833).release().perform()
+driver.implicitly_wait(10)
+TouchAction(driver).tap(x=272, y=1561).perform()
+driver.implicitly_wait(10)
+
+driver.quit()
+
